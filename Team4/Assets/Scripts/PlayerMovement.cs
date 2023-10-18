59c39f2825b9f17d1150da01d8a3587b0ca37f92
Arystos <arystotelos@gmail.com>
2023-10-18 16:32:56 +0200

    🚫 Square Status
    I created a script that should be placed on every enemy and items and player(s) that raycast under him and change the status isOcupied to the Square where he stay

--- Team4/Assets/Scripts/PlayerMovement.cs
+++ Team4/Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,14 @@ public class PlayerMovement : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.CompareTag("Box"))
+
+                if (hit.collider.CompareTag("Square")) // Assicurati di assegnare il tag "Cube" ai tuoi cubi
                 {
-                    MoveTo(hit.collider.transform.position);
+                    SquareStatus squareStatus = hit.collider.GetComponent<SquareStatus>();
+                    if (squareStatus != null && !squareStatus.isOccupied)
+                    {
+                        MoveTo(hit.collider.transform.position);
+                    }
                 }
             }
         }
@@ -20,6 +25,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void MoveTo(Vector3 targetPosition)
     {
-        transform.position = targetPosition;
+        Occupier occupier = GetComponent<Occupier>();
+        if (occupier)
+        {
+            occupier.MoveTo(targetPosition);
+        }
     }
 }
